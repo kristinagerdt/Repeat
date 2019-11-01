a3cd3fa8d927497ebd8e6d28c177f1d6bbddb697
kristinagerdt <gerdt.kristina@gmail.com>
2019-11-01 16:35:06 +0100

    gh-14 solution for Add

---
+++ test/cs/theJourneyBegins/AddTest.java
@@ -0,0 +1,56 @@
+package cs.theJourneyBegins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddTest {
+
+    private int a;
+    private int b;
+
+    @Test
+    public void testAdd() {
+        a = 4;
+        b = 2;
+        int actual = a + b;
+        int expected = Add.add(a, b);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNullAdd() {
+        a = 0;
+        b = 0;
+        int actual = a + b;
+        int expected = Add.add(a, b);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNegativeAdd() {
+        a = -1;
+        b = -3;
+        int actual = a + b;
+        int expected = Add.add(a, b);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBigIntsAdd() {
+        a = 10000;
+        b = 20000;
+        int actual = a + b;
+        int expected = Add.add(a, b);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMixAdd() {
+        a = -100000;
+        b = 20000;
+        int actual = a + b;
+        int expected = Add.add(a, b);
+        assertEquals(expected, actual);
+    }
+}
