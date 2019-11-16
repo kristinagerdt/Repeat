749e9b16d225aed38612cb8a9c0570c06917dab4
kristinagerdt <gerdt.kristina@gmail.com>
2019-11-16 17:46:49 +0100

    gh-43 solution for ArrayMaximalAdjacentDifference

---
+++ src/cs/islandOfKnowledge/ArrayMaximalAdjacentDifference.java
@@ -0,0 +1,22 @@
+package cs.islandOfKnowledge;
+
+import java.util.stream.IntStream;
+
+public class ArrayMaximalAdjacentDifference {
+    public static int arrayMaximalAdjacentDifference(int[] inputArray) {
+        int maxDiff = 0;
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            int tempDiff = Math.abs(inputArray[i] - inputArray[i + 1]);
+            if (tempDiff > maxDiff) maxDiff = tempDiff;
+        }
+        return maxDiff;
+    }
+
+    public static int arrayMaximalAdjacentDifferenceStream(int[] inputArray) {
+        return IntStream
+                .range(0, inputArray.length - 1)
+                .map(i -> Math.abs(inputArray[i] - inputArray[i + 1]))
+                .max()
+                .orElse(0);
+    }
+}
